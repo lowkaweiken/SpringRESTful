f1f62825953814c2b1c0efbd63d228c21edf6049
Ken Low <0118525@kdu-online.com>
2019-07-02 22:52:12 +0800

    First Commit

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,21 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+//    The @RequestMapping annotation ensures that HTTP requests to /greeting are mapped to the greeting() method.
+//    @RequestParam binds the value of the query string parameter name into the name parameter of the greeting() method.
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
